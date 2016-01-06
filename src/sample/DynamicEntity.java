fe94fd6ce60fee48d0017f909f4130f0f3ba7b1c
ikrysinska <izabela_krysinska@outlook.com>
2016-01-06 22:56:43 +0100

    Entities

--- src/sample/DynamicEntity.java
+++ src/sample/DynamicEntity.java
@@ -6,6 +6,10 @@ package sample;
 public class DynamicEntity extends StaticEntity{
     public double  vel_x;
     public double vel_y;
+    public double movement_vector_length = Math.sqrt(Math.pow(this.vel_x,2) + Math.pow(this.vel_y,2));
+
+    public Point movement_vector_top = new Point(this.vel_x + this.point.pos_x, this.vel_y + this.point.pos_y);
+
     public double speed;
     public double gravity;
     public enum Direction_enum{RIGHT, LEFT ,UP, DOWN};
@@ -19,7 +23,7 @@ public class DynamicEntity extends StaticEntity{
         private void setStatus(boolean new_status){
             intersect = new_status;
         }
-        private Intersect_enum(boolean ifIntersect){
+        Intersect_enum(boolean ifIntersect){
             intersect = ifIntersect;
         }
     };
@@ -28,62 +32,107 @@ public class DynamicEntity extends StaticEntity{
 
     public void calculateNewPosition(Direction_enum direction){
 
-        if(direction.equals(Direction_enum.RIGHT)) { pos_x += vel_x; }
-        if(direction.equals(Direction_enum.LEFT))  { pos_x -= vel_x; }
-        if(direction.equals(Direction_enum.DOWN))  { pos_y -= vel_y; }
-        if(direction.equals(Direction_enum.UP))    { pos_y += vel_y; }
+        if(direction.equals(Direction_enum.RIGHT)) { point.pos_x += vel_x; }
+        if(direction.equals(Direction_enum.LEFT))  { point.pos_x -= vel_x; }
+        if(direction.equals(Direction_enum.DOWN))  { point.pos_y -= vel_y; }
+        if(direction.equals(Direction_enum.UP))    { point.pos_y += vel_y; }
     }
     public void detectStaticCollison( ){
         this.intersect();
-
+        if (Intersect_enum.LEFT_WALL.intersect)
+            collisionWithLeftWall();
+        if(Intersect_enum.RIGHT_WALL.intersect)
+            collisionWithRightWall();
+        if(Intersect_enum.CEILING.intersect)
+            collisionWithCeiling();
+        if(Intersect_enum.GROUND.intersect){
+            collisionWithGround();
+        }
+        if(Intersect_enum.NET.intersect)
+            collisionWithNet();
 
     }
 
-    public void collisionWithLeftWall(){
-
+    private void collisionWithLeftWall(){
+        this.vel_x = - vel_x;
     }
+    private void collisionWithRightWall(){
+        this.vel_x = - vel_x;
+    }
+    private void collisionWithCeiling(){
+        this.vel_y = - vel_y;
+    }
+    private void collisionWithGround(){
+        //end game or sth ;p
+    }
+    private void collisionWithNet(){
+        if (this.center_point.pos_y > list_of_staticEntity[4].point.pos_y){
+            this.vel_x = -vel_x;
+        }
+        //else
+    }
+
     public double distanceBetweenTwoPoints(int x_1,int y_1,int x_2, int y_2){
         int x = Math.abs(x_1 - x_2);
         int y = Math.abs(y_1 - y_2);
         return Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
 
     }
+    public Point closestPointOnLine(Point point_line1,Point point_line2,Point point_center ){
+        double A = point_line2.pos_y -point_line1.pos_y;
+        double B = point_line1.pos_x - point_line2.pos_x;
+        double C1 = (point_line2.pos_y - point_line1.pos_y)*point_line1.pos_x + (point_line1.pos_x - point_line2.pos_x)*point_line1.pos_y;
+        double C2 = -B*point_center.pos_x + A*point_center.pos_y;
+        double det = A*A - -B*B;
+        double cx = 0;
+        double cy = 0;
+        if(det != 0){
+            cx = (A*C1 - B*C2)/det;
+            cy = (A*C2 - -B*C1)/det;
+        }else{
+            cx = point_center.pos_x;
+            cy = point_center.pos_y;
+        }
+        return new Point(cx, cy);
+    }
     public boolean intersect(){
         StaticEntity static_entity= new StaticEntity();
         Intersect_enum intersect_enum = Intersect_enum.LEFT_WALL;
-        if(this.pos_x<static_entity.list_of_staticEntity[0].pos_x){ // collison with left wall
+        if(this.point.pos_x<static_entity.list_of_staticEntity[0].point.pos_x){ // collison with left wall
             intersect_enum.setStatus(true);
         }
-        if(this.pos_x + this.width > static_entity.list_of_staticEntity[1].pos_x){ // collision with right wall
+        if( this.point.pos_x + this.width > static_entity.list_of_staticEntity[1].point.pos_x){ // collision with right wall
             intersect_enum = Intersect_enum.RIGHT_WALL;
             intersect_enum.setStatus(true);
         }
-        if(this.pos_y<static_entity.list_of_staticEntity[2].pos_y){ // collision with ceiling
+        if(this.point.pos_y<static_entity.list_of_staticEntity[2].point.pos_y){ // collision with ceiling
             intersect_enum = Intersect_enum.CEILING;
             intersect_enum.setStatus(true);
         }
-        if(this.pos_y + this.height> static_entity.list_of_staticEntity[3].pos_y){ // collison with ground
+        if(this.point.pos_y + this.height> static_entity.list_of_staticEntity[3].point.pos_y){ // collison with ground
             intersect_enum = Intersect_enum.GROUND;
             intersect_enum.setStatus(true);
         }
-        if (this.pos_y + this.height > static_entity.list_of_staticEntity[4].pos_y){  // collision with net
-            if(this.pos_x + this.width > static_entity.list_of_staticEntity[4].pos_x &&
-                    this.pos_x < static_entity.list_of_staticEntity[4].pos_x +static_entity.list_of_staticEntity[4].width){
+        if (this.point.pos_y + this.height > static_entity.list_of_staticEntity[4].point.pos_y){  // collision with net
+            if(this.point.pos_x + this.width > static_entity.list_of_staticEntity[4].point.pos_x &&
+                    this.point.pos_x < static_entity.list_of_staticEntity[4].point.pos_x +static_entity.list_of_staticEntity[4].width){
                 intersect_enum = Intersect_enum.NET;
                 intersect_enum.setStatus(true);
             }
         }
-        double distance = distanceBetweenTwoPoints(this.center_x,this.center_y,list_of_staticEntity[4].pos_x,list_of_staticEntity[4].pos_y);
+        Point closest_point = closestPointOnLine(this.point,this.movement_vector_top, this.net_top_center);
+
+        /*double distance = distanceBetweenTwoPoints(this.center_x,this.center_y,list_of_staticEntity[4].point.pos_x,list_of_staticEntity[4].point.pos_y);
         if (distance < this.width/2){
             intersect_enum = Intersect_enum.NET;
             intersect_enum.setStatus(true);
         }
         distance = distanceBetweenTwoPoints(this.center_x,this.center_y,
-                list_of_staticEntity[4].pos_x + list_of_staticEntity[4].width,list_of_staticEntity[4].pos_y);
+                list_of_staticEntity[4].point.pos_x + list_of_staticEntity[4].width,list_of_staticEntity[4].point.pos_y);
         if (distance < this.width/2){
             intersect_enum = Intersect_enum.NET;
             intersect_enum.setStatus(true);
-        }
+        }*/
 
 
         return true;
--- src/sample/StaticEntity.java
+++ src/sample/StaticEntity.java
@@ -4,18 +4,32 @@ import java.lang.String;
 /**
  * Created by Izabela on 06.01.2016.
  */
+
 public class StaticEntity {
-    public int pos_x;
-    public int pos_y;
+
+    StaticEntity(){//w scenie gameplay wywolywac konstruktory w kolejnosci left_wall,right_wall,celling,net,ground
+        addStaticEntity();
+    }
+
+    public  class Point{
+        public double pos_x;
+        public double pos_y;
+        Point(double new_pos_x, double new_pos_y){
+            pos_x = new_pos_x;
+            pos_y = new_pos_y;
+        }
+    }
+    Point point;
+    Point net_top_center = new Point(this.list_of_staticEntity[4].width/2 + this.list_of_staticEntity[4].point.pos_x,
+            this.list_of_staticEntity[4].width/2 + this.list_of_staticEntity[4].point.pos_y);
+    Point center_point = new Point( point.pos_x + this.width/2,point.pos_y + this.height/2);
     public int width;
     public int height;
-    public int center_x = pos_x + width/2;
-    public int center_y = pos_y + height/2;
     Image image;
     public String path;
     public StaticEntity[] list_of_staticEntity;
 
-    public  void addStaticEntity(){ //w scenie gameplay wywolywac konstruktory w kolejnosci left_wall,right_wall,celling,net,ground
+    public  void addStaticEntity(){
         int elem_counter = list_of_staticEntity.length;
         list_of_staticEntity[elem_counter] = this;
     }
