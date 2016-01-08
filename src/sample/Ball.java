570d8f2d4f1f34a3d04825e891047ab8291b4e09
mateuszl1995 <mateusz.p.lewandowski@student.put.poznan.pl>
2016-01-08 22:28:18 +0100

    Update Ball.java

--- src/sample/Ball.java
+++ src/sample/Ball.java
@@ -11,13 +11,13 @@ public class Ball extends DynamicEntity {
     private Point startPos;
     private double startVel; // velocity after bounce the ball
 
-    Ball(double posX, double posY, double rad, double vel) { // px, py - coordinates of ball center, rad - radius, vel - velocity after bounce
+    Ball(double posX, double posY, double rad, double Vel) { // px, py - coordinates of ball center, rad - radius, Vel - velocity after bounce
         center.pos_x = posX;
         center.pos_y = posY;
         radius = rad;
         startPos.pos_x = posX - rad;
         startPos.pos_y = posY - rad;
-        startVel = -vel;
+        startVel = -Vel;
         setStartPos();
     }
 
@@ -32,13 +32,14 @@ public class Ball extends DynamicEntity {
     }
 
     public int detectDynamicCollision(Animal animal) {    // returns 1 if collision, else returns 0
+        if(GamePlay.stop()) return 0;   // case when the ball touches the ground and animal tries to push ball under ground.
         countCenter();
         // solve distance
-        double dx = center.pos_x - animal.center.pos_x;
-        double dy = center.pos_y - animal.center.pos_y;
+        double dx = center.pos_x - animal.getCenter().pos_x;
+        double dy = center.pos_y - animal.getCenter().pos_y;
         double distance = Math.sqrt(dx * dx + dy * dy);
 
-        if (distance <= radius + animal.radius) { // if collision then count new vel_x and vel_y
+        if (distance <= radius + animal.getRadius()) { // if collision then count new vel_x and vel_y
             // assuming that mass of animal is much greater than mass of ball
             vel_x = startVel * (dx / distance);
             vel_y = startVel * (dy / distance);
