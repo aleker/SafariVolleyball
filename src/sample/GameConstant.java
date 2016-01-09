e77b2b724fa37b8ee93d65431cedcb92e68ca48b
ikrysinska <izabela_krysinska@outlook.com>
2016-01-09 16:04:23 +0100

    final

--- src/sample/GameConstant.java
+++ src/sample/GameConstant.java
@@ -6,7 +6,7 @@ package sample;
 public class GameConstant {
     double C_VEL_X = 8;
     double C_VEL_Y = 8;
-    double C_SPEED = 8;
+    double C_SPEED = - 8;
     double C_GRAVITY = 0.1;
     double C_START_POS_X = 0;
     double C_START_POS_Y = 0;
