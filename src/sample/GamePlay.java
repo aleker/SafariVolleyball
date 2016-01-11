fa78be77815ae5844a8fd2db17c1d2053cfbfe6f
aleker <aleker@users.noreply.github.com>
2016-01-11 18:54:01 +0100

    Final Menu.

--- src/sample/GamePlay.java
+++ src/sample/GamePlay.java
@@ -10,9 +10,6 @@ import javafx.scene.layout.*;
 
 public class GamePlay extends SceneWrapper {
 
-    int left_index;
-    int right_index;
-    Player list_of_players[];
     int points[];
     private static boolean playing;
 
@@ -21,32 +18,8 @@ public class GamePlay extends SceneWrapper {
 
     }
 
-    public int getLeft_index() {
-        return left_index;
-    }
-
-    public void setLeft_index(int left_index) {
-        this.left_index = left_index;
-    }
-
-    public int getRight_index() {
-        return right_index;
-    }
-
-    public void setRight_index(int right_index) {
-        this.right_index = right_index;
-    }
-
     @Override
     public void initialize() {
-        System.out.print("left_index ");
-        System.out.println(left_index);
-        System.out.print("right_index ");
-        System.out.println(right_index);
-        this.addBackground(new Image("file:src/Pictures/background.png"));
-        points = new int[2];
-        PlayerList playerList = new PlayerList();
-        //playerList.newPlayer(left_index, Player.LEFT_SIDE);   // newPlayer(left_index, Player.LEFT_SIDE);
 
     }
 
@@ -64,8 +37,6 @@ public class GamePlay extends SceneWrapper {
         return !playing;
     }
     public void createEntities() {
-        Ball ball = new Ball(200, 200, 20, 20);
-        StaticEntity net = new StaticEntity("file:src/Pictures/net.png", 400, 200);
 
     }
     public void createPlayer() {}
--- src/sample/Menu.java
+++ src/sample/Menu.java
@@ -5,6 +5,8 @@ import com.sun.javafx.geom.transform.BaseTransform;
 import com.sun.javafx.jmx.MXNodeAlgorithm;
 import com.sun.javafx.jmx.MXNodeAlgorithmContext;
 import com.sun.javafx.sg.prism.NGNode;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
@@ -28,20 +30,20 @@ public class Menu extends SceneWrapper {
     @Override
     public void initialize() {
         this.addBackground(new Image("file:src/Pictures/background.png"));
-        final GridPane grid = (GridPane) getRoot();
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setHgap(5);
+        grid.setVgap(5);
+        grid.setLayoutX(250);
+        grid.setLayoutY(150);
 
         // CHANGE PLAYER
         // labels
         final Label leftPlayerLabel = new Label("Left Player:");
         leftPlayerLabel.setStyle("-fx-font: 16 verdana; -fx-text-fill: snow");
-        leftPlayerLabel.setLayoutX(270);
-        leftPlayerLabel.setLayoutY(200);
-        //this.addEntity(leftPlayerLabel);
         final Label rightPlayerLabel = new Label("Right Player:");
         rightPlayerLabel.setStyle("-fx-font: 16 verdana; -fx-text-fill: snow");
-        rightPlayerLabel.setLayoutX(270);
-        rightPlayerLabel.setLayoutY(250);
-        //this.addEntity(rightPlayerLabel);
         grid.add(leftPlayerLabel, 0, 1);
         grid.add(rightPlayerLabel, 0, 2);
 
@@ -54,42 +56,23 @@ public class Menu extends SceneWrapper {
         }
         leftPlayerComboBox.setValue(leftPlayerComboBox.getItems().get(0));
         rightPlayerComboBox.setValue(rightPlayerComboBox.getItems().get(0));
-        leftPlayerComboBox.setLayoutX(400);
-        leftPlayerComboBox.setLayoutY(200);
-        rightPlayerComboBox.setLayoutX(400);
-        rightPlayerComboBox.setLayoutY(250);
-        //this.addEntity(leftPlayerComboBox);
-        //this.addEntity(rightPlayerComboBox);
         grid.add(leftPlayerComboBox, 1, 1);
         grid.add(rightPlayerComboBox, 1, 2);
 
 
         // NEW GAME
         Button b_newGame = new Button("Start");
-        b_newGame.setOnAction(e -> {
-            System.out.print("left_index ");
-            System.out.println(leftPlayerComboBox.getSelectionModel().getSelectedIndex());
-            System.out.print("right_index ");
-            System.out.println(rightPlayerComboBox.getSelectionModel().getSelectedIndex());
-            GamePlay game = new GamePlay(new Group(), this.game, 800, 600);
-            game.setLeft_index(leftPlayerComboBox.getSelectionModel().getSelectedIndex());
-            game.setRight_index(rightPlayerComboBox.getSelectionModel().getSelectedIndex());
-            this.exit(game);
-        });
+        b_newGame.setOnAction(e -> this.exit(new GamePlay(new Group(), this.game, 800, 600)));
         b_newGame.setStyle("-fx-focus-color: #FF9933; -fx-font: 18 verdana; -fx-base: #FFFF66;");
-        b_newGame.setLayoutX(400);
-        b_newGame.setLayoutY(300);
-        //this.addEntity(b_newGame);
-        grid.add(b_newGame, 0, 3, 2, 1);
+        grid.add(b_newGame, 1, 3, 2, 1);
 
         // EXIT
         Button b_close = new Button("Exit");
         b_close.setOnAction(e -> this.quit());
         b_close.setStyle("-fx-focus-color: #FF9933; -fx-font: 18 verdana; -fx-base: #FFFF66;");
-        b_close.setLayoutX(270);
-        b_close.setLayoutY(300);
-        //this.addEntity(b_close);
-        grid.add(b_close, 0, 3, 2, 2);
+        grid.add(b_close, 0, 3, 2, 1);
+
+        this.group.getChildren().add(grid);
     }
 
     @Override
@@ -102,45 +85,6 @@ public class Menu extends SceneWrapper {
 
     }
 
-    public void changePlayer() {}
     public void quit() { this.game.stage.close(); }
 
 }
-
-/*      super(new GridPane(), 640, 480);
-        final GridPane grid = (GridPane) getRoot();
-        grid.setAlignment(Pos.CENTER);
-        grid.setPadding(new Insets(10, 10, 10, 10));
-        grid.setHgap(5);
-        grid.setVgap(5);
-
-        final Text title = new Text("Safari Volleyball");
-        title.setFont(Font.font("Tahoma", FontWeight.BOLD, 20));
-        grid.add(title, 0, 0, 2, 1);
-
-        final Label leftPlayerLabel = new Label("Left Player:");
-        final Label rightPlayerLabel = new Label("Right Player:");
-        grid.add(leftPlayerLabel, 0, 1);
-        grid.add(rightPlayerLabel, 0, 2);
-
-        final ComboBox<String> leftPlayerComboBox = new ComboBox<String>();
-        final ComboBox<String> rightPlayerComboBox = new ComboBox<String>();
-        leftPlayerComboBox.getItems().addAll("Human (WSAD)");
-        leftPlayerComboBox.setValue(leftPlayerComboBox.getItems().get(0));
-        rightPlayerComboBox.getItems().addAll("Human (Arrows)", "AI1", "AI2");
-        rightPlayerComboBox.setValue(rightPlayerComboBox.getItems().get(0));
-        grid.add(leftPlayerComboBox, 1, 1);
-        grid.add(rightPlayerComboBox, 1, 2);
-
-        final Button playButton = new Button("Play!");
-        playButton.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent ae) {
-                System.out.println("Left Player: " + leftPlayerComboBox.getValue());
-                System.out.println("Right Player: " + rightPlayerComboBox.getValue());
-            }
-        });
-        grid.add(playButton, 0, 3, 2, 1);
-
-        final BackgroundImage backgroundImage = loadBackgroundImage("sample/background.png");
-        grid.setBackground(new Background(backgroundImage));
-        // no fills? */
