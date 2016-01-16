4e788849f839e4e16b760729851d5442d6aa4644
aleksander-zn <aleksander-zn@users.noreply.github.com>
2016-01-16 16:54:47 +0100

    Ball physics corrected (deltaTime).

--- src/sample/GameConstant.java
+++ src/sample/GameConstant.java
@@ -4,10 +4,11 @@ package sample;
  * Created by Izabela on 08.01.2016.
  */
 public class GameConstant {
-    public static final double C_VEL_X = 2;
-    public static final double C_VEL_Y = 7;
-    public static final double C_SPEED = - 7;
-    public static final double C_GRAVITY = 0.03;
+    public static final double C_SPEED = -180.0;
+    private static final double C_ANGLE = 180.0 * Math.PI / 180.0;
+    public static final double C_VEL_X = C_SPEED * Math.cos(C_ANGLE);
+    public static final double C_VEL_Y = C_SPEED * Math.sin(C_ANGLE);
+    public static final double C_GRAVITY = 200;
     public static final double C_START_POS_X = 400;
     public static final double C_START_POS_Y = 0;
 
@@ -19,7 +20,7 @@ public class GameConstant {
     public static final double ANIMAL_JUMP_SPEED = 10.0;
 
     public static final int ANIMAL_JUMP_HEIGHT = 200;
-    public static final double ANIMAL_GRAVITY = 200;
+    public static final double ANIMAL_GRAVITY = C_GRAVITY; //200;
     public static final double ANIMAL_SCALE = 0.85;
 
     public static final double BALL_SCALE = 0.85;
