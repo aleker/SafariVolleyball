a81041e4c53c9360c3ddda48cbac9747d1cdcbb6
Joanna Sołomiewicz <joanna.solomiewicz@student.put.poznan.pl>
2016-01-17 22:58:21 +0100

    New methods
    Method for detecting if ball is on my field.
    Method for setting animal in the middle of the field, when the ball is not on his side.

--- src/sample/AI_Joanna.java
+++ src/sample/AI_Joanna.java
@@ -1,7 +1,54 @@
 package sample;
 
 public class AI_Joanna extends Player {
+
+    private int side;
+    private double time;
+    private int LEFT = 0;
+    private int RIGHT = 1;
+    private int UP = 2;
+    private int DOWN = 3;
+    private int middle;
+    private Point ballCoordinates;
+
     public AI_Joanna(int side) {
         super(side);
+        this.side = side;
+        middle = GameConstant.RIGHT_WALL_X/2;
+    }
+
+    @Override
+    public void moveDecision(int direction, double deltaTime, Point ball_point) {
+
+        time = deltaTime;
+
+        ballCoordinates = ball_point;
+        boolean ballOnMySide = isBallOnMySide();
+    }
+
+    private boolean isBallOnMySide() {
+        if(ballCoordinates.pos_x > middle) {
+            if(side == LEFT) return false;
+            else return true;
+        }
+        else {
+            if(side == LEFT) return true;
+            else return false;
+        }
+    }
+
+    private void prepare() {
+        if(side == LEFT) {
+            if(animal.center_point.pos_x < middle/2)
+                animal.move(animal.DIR_RIGHT, time);
+            else if(animal.center_point.pos_x > middle/2)
+                animal.move(animal.DIR_LEFT, time);
+        }
+        else {
+            if(animal.center_point.pos_x < middle + middle/2)
+                animal.move(animal.DIR_RIGHT, time);
+            else if(animal.center_point.pos_x > middle + middle/2)
+                animal.move(animal.DIR_LEFT, time);
+        }
     }
 }
